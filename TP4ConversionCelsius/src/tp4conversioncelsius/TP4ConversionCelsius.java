704ab5a4dd442beda3af78c4f5d9d4c4dbe16599
Gabriel Torrez <110358252+gabriel-1993@users.noreply.github.com>
2023-08-18 23:43:39 -0300

    TP4ConversionCelsiusAfahrenheit

---
+++ TP4ConversionCelsius/src/tp4conversioncelsius/TP4ConversionCelsius.java
@@ -0,0 +1,27 @@
+package tp4conversioncelsius;
+
+public class TP4ConversionCelsius {
+
+    public static void main(String[] args) {
+     // Ejercicio 2:
+
+/*Crea una aplicación GUI en Java que simule un convertidor de unidades de temperatura. La interfaz debe contener los siguientes elementos:
+
+Un campo de texto (JTextField) para que el usuario pueda ingresar la temperatura en grados Celsius.
+Un botón (JButton) con el texto "Convertir".
+Cuando el usuario ingrese la temperatura en grados Celsius y haga clic en el botón "Convertir", la aplicación debe realizar la conversión de Celsius a Fahrenheit y mostrar el resultado en un cuadro de diálogo emergente.
+
+La fórmula para convertir de grados Celsius a Fahrenheit es: Fahrenheit = Celsius * 9/5 + 32.
+
+Recuerda utilizar la biblioteca de componentes gráficos de Java (Swing) para crear la interfaz y gestionar el evento del botón.
+
+Subir proyecto a GitHub y enviar link al repositorio.
+
+Abierta hasta : 31/08/2023 23:59*/
+   
+    
+    
+    
+    }
+    
+}

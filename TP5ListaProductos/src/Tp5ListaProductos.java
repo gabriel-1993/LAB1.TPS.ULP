eac0585f0d7bd867013eb20845756ce35a0afd8c
Gabriel Torrez <110358252+gabriel-1993@users.noreply.github.com>
2023-08-19 01:45:05 -0300

    continuara...

---
+++ TP5ListaProductos/src/Tp5ListaProductos.java
@@ -0,0 +1,18 @@
+
+public class Tp5ListaProductos {
+
+    /* Seria TP 3 de la unidad 2, los venia contando desde la unidad 1 por eso esta como TP5*/
+    public static void main(String[] args) {
+/*Ejercicio 3 Unidad 2:
+Crea una aplicación GUI en Java que gestione una lista de productos. La interfaz debe contener los siguientes elementos:
+Un ComboBox (JComboBox) que permita al usuario seleccionar una categoría de producto de una lista predefinida (por ejemplo, electrónica, ropa, alimentos, etc.).
+Un botón "Agregar producto" (JButton) que permita al usuario agregar un nuevo producto a la lista.
+Una tabla (JTable) que muestre los productos agregados, con columnas para el nombre del producto, la categoría y el precio.
+Cuando el usuario seleccione una categoría en el ComboBox y haga clic en el botón "Agregar producto", el programa debe permitir al usuario ingresar el nombre y el precio del producto en campos de texto separados. Luego, debe agregar el producto a la tabla con la categoría seleccionada.
+Además, la tabla debe ser actualizada automáticamente cada vez que se agrega un nuevo producto.
+Subir proyecto a GitHub y enviar link del repositorio.
+Abierta hasta : 31/08/2023 23:59
+    */    
+    }
+    
+}
